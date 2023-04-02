c3c883d3bf2e8533de1fecc0fd9bef348046cd3b
PARK SEHYUN <87633056+AOBANG@users.noreply.github.com>
2023-04-02 14:56:15 +0900

    Create bfs.py

---
+++ BFS_Algorithm/bfs.py
@@ -0,0 +1,23 @@
+# 그래프 탐색 - 너비 우선 탐색(BFS: Breadth First Search)
+# 시작 정점에서 인접한 노드를 먼저 방문하고 멀리 떨어져 있는 정점을 나중에 방문
+# 모든 노드를 탐색하는 것보다 최소 비용, 최단 경로, 임의의 경로를 찾아야 하는 경우에 사용
+# 큐(Queue) 를 이용하여 구현
+# 1. 시작 노드를 Queue에 삽입하고 visit[시작] = true 로 방문 처리
+# 2. Queue의 front를 현재 노드 변수에 저장하고 pop()
+# 3. Queue의 현재 노드에 방문하지 않은 인접 노드가 있다면 Queue에 삽입하고 방문 처리
+# 4. Queue가 빌 때까지 반복
+
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True # 현재 노드 방문 처리
+
+    while queue: # 아직 큐에 노드가 있다면
+        cur = queue.popleft() # 하나를 빼와 현재 노드 변수에 저장
+        print(cur, end=' ') # 출력
+
+        for next in graph[cur]: 
+            if not visited[next]: # 방문하지 않은 노드라면
+                queue.append(next) # 큐에 삽입
+                visited[next] = True # 방문 처리
