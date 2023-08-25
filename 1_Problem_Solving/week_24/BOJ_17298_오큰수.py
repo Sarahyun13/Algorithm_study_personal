b2d02b667aba0c1d2b78484f60d367480d430a77
PARK SEHYUN <sehyunpark0103@gmail.com>
2023-08-26 00:17:28 +0900

    Update BOJ_17298_오큰수.py

--- 1_Problem_Solving/week_24/BOJ_17298_오큰수.py
+++ 1_Problem_Solving/week_24/BOJ_17298_오큰수.py
@@ -4,4 +4,13 @@ input = sys.stdin.readline
 
 N = int(input())
 seq = list(map(int, input().split()))
-NGE = []
+NGE = [-1] * N
+stack = []
+
+for i in range(N):
+    while stack and (stack[-1][0] < seq[i]):
+        tmp, idx = stack.pop()
+        NGE[idx] = seq[i]
+    stack.append((seq[i], i))
+
+print(*NGE)
