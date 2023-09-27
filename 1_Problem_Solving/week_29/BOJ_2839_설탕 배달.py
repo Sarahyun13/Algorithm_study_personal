99589e861f21906c912c36add4bf00102d8ee2be
PARK SEHYUN <sehyunpark0103@gmail.com>
2023-09-27 23:57:53 +0900

    Update BOJ_2839_설탕 배달.py

--- 1_Problem_Solving/week_29/BOJ_2839_설탕 배달.py
+++ 1_Problem_Solving/week_29/BOJ_2839_설탕 배달.py
@@ -4,6 +4,8 @@ input = sys.stdin.readline
 
 N = int(input())
 
+"""
+# 그리디 알고리즘
 count = 0
 while True:
     if N % 5 == 0:
@@ -21,3 +23,12 @@ if N == 0:
     print(count)
 else:
     print(-1)
+"""
+
+# 다이나믹 프로그래밍
+dp = [-1] * 5001
+dp[3] = 1
+dp[5] = 1
+for i in range(6, 5001):
+    if i % 5 == 0:
+        dp[i] = dp[i - 5] + 1
