f9b94d5eeec1db63ddabd2ec68bef9a103feea9f
PARK SEHYUN <sehyunpark0103@gmail.com>
2023-09-28 15:17:33 +0900

    Update BOJ_2839_설탕 배달.py

--- 1_Problem_Solving/week_29/BOJ_2839_설탕 배달.py
+++ 1_Problem_Solving/week_29/BOJ_2839_설탕 배달.py
@@ -30,5 +30,13 @@ dp = [-1] * 5001
 dp[3] = 1
 dp[5] = 1
 for i in range(6, 5001):
-    if i % 5 == 0:
+    if dp[i - 3] == -1 and dp[i - 5] == -1:
+        dp[i] = -1
+    elif dp[i - 3] == -1 and dp[i - 5] > 0:
         dp[i] = dp[i - 5] + 1
+    elif dp[i - 3] > 0 and dp[i - 5] == -1:
+        dp[i] = dp[i - 3] + 1
+    elif dp[i - 3] > 0 and dp[i - 5] > 0:
+        dp[i] = min(dp[i - 3], dp[i - 5]) + 1
+
+print(dp[N])
