bbed1c6854b24b3eaa519f3b48332e0db60afae9
PARK SEHYUN <sehyunpark0103@gmail.com>
2023-11-18 22:14:36 +0900

    Create BOJ_1240_노드사이의 거리.py

---
+++ 1_Problem_Solving/week_36/BOJ_1240_노드사이의 거리.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def bfs(a, b):
+    queue = deque()
+    distance = [-1] * (N + 1)
+    queue.append(a)
+    distance[a] = 0
+
+    while queue:
+        now = queue.popleft()
+        if now == b:
+            return distance[b]
+
+        for next, dist in tree[now]:
+            if distance[next] == -1:
+                distance[next] = distance[now] + dist
+                queue.append(next)
+
+
+N, M = map(int, input().split())
+tree = [[] * (N + 1) for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b, dist = map(int, input().split())
+    tree[a].append((b, dist))
+    tree[b].append((a, dist))
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    print(bfs(a, b))
