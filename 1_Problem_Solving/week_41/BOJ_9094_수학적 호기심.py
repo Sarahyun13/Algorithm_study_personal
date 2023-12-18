54fd4034e7b89eb04ae67867d5747ae59eec4366
PARK SEHYUN <sehyunpark0103@gmail.com>
2023-12-18 15:13:39 +0900

    Create BOJ_9094_수학적 호기심.py

---
+++ 1_Problem_Solving/week_41/BOJ_9094_수학적 호기심.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    n, m = map(int, input().split())
+    count = 0
+    for a in range(1, n - 1):
+        for b in range(a + 1, n):
+            if (a**2 + b**2 + m) % (a * b) == 0:
+                count += 1
+
+    print(count)
