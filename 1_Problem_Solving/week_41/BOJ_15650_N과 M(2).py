3c915471368bfac5913312c54462aa47c964457a
PARK SEHYUN <sehyunpark0103@gmail.com>
2023-12-21 20:41:14 +0900

    Update BOJ_15650_N과 M(2).py

--- 1_Problem_Solving/week_41/BOJ_15650_N과 M(2).py
+++ 1_Problem_Solving/week_41/BOJ_15650_N과 M(2).py
@@ -1,3 +1,22 @@
 import sys
 
 input = sys.stdin.readline
+
+
+def dfs(start):
+    if len(seq) == M:
+        print(" ".join(map(str, seq)))
+        return
+
+    for i in range(start, N + 1):
+        if i in seq:
+            continue
+        else:
+            seq.append(i)
+            dfs(i + 1)
+            seq.pop()
+
+
+N, M = map(int, input().split())
+seq = []
+dfs(1)
