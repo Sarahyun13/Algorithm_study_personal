d8fa193785a9dd3dafa540ada217fc678a54e7b5
PARK SEHYUN <sehyunpark0103@gmail.com>
2023-12-26 23:33:50 +0900

    Create BOJ_15655_N과 M(6).py

---
+++ 1_Problem_Solving/week_42/BOJ_15655_N과 M(6).py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(idx):
+    if len(seq) == M:
+        print(" ".join(map(str, seq)))
+        return
+
+    for i in range(idx, N):
+        seq.append(nums[i])
+        dfs(i + 1)
+        seq.pop()
+
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+seq = []
+dfs(0)
