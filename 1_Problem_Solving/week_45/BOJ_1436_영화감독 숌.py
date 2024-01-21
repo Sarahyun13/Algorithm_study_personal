a8d1f707a937bce50f20257a7d51a1f7f4f3bce7
PARK SEHYUN <sehyunpark0103@gmail.com>
2024-01-21 22:53:37 +0900

    Create BOJ_1436_영화감독 숌.py

---
+++ 1_Problem_Solving/week_45/BOJ_1436_영화감독 숌.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+num = 666
+count = 0
+
+while True:
+    if '666' in str(num):
+        count += 1
+
+    if count == N:
+        print(num)
+        break
+
+    num += 1
