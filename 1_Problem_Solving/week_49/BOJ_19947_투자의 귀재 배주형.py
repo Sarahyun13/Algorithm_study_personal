d40d2d5f7487e40afb02cb7fc165ff441b94b56c
PARK SEHYUN <sehyunpark0103@gmail.com>
2024-02-17 19:20:28 +0900

    Update BOJ_19947_투자의 귀재 배주형.py

--- 1_Problem_Solving/week_49/BOJ_19947_투자의 귀재 배주형.py
+++ 1_Problem_Solving/week_49/BOJ_19947_투자의 귀재 배주형.py
@@ -3,4 +3,16 @@ import sys
 input = sys.stdin.readline
 
 H, Y = map(int, input().split())
-print()
+
+# 투자 방식을 매년 바꿀 수 있음에 주의
+dp = [0] * (Y+1)
+dp[0] = H
+for year in range(1, Y+1):
+    if year >= 5:
+        dp[year] = int(max(dp[year-1] * 1.05, dp[year-3] * 1.2, dp[year-5] * 1.35))
+    elif year >= 3:
+        dp[year] = int(max(dp[year-1] * 1.05, dp[year-3] * 1.2))
+    else:
+        dp[year] = int(dp[year - 1] * 1.05)
+
+print(dp[Y])
