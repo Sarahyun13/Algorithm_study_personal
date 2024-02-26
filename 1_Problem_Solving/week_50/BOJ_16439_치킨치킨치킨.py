efd74e58797109621605f59a3e8aacfec6a6d97a
PARK SEHYUN <sehyunpark0103@gmail.com>
2024-02-26 18:12:41 +0900

    Update BOJ_16439_치킨치킨치킨.py

--- 1_Problem_Solving/week_50/BOJ_16439_치킨치킨치킨.py
+++ 1_Problem_Solving/week_50/BOJ_16439_치킨치킨치킨.py
@@ -1,5 +1,17 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
-N = int(input())
+N, M = map(int, input().split())
+prefer = [list(map(int, input().split())) for _ in range(N)]
+
+result = []
+chicken = list(i for i in range(M))
+for comb in combinations(chicken, 3):
+    total = 0
+    for student in prefer:
+        total += max(student[comb[0]], student[comb[1]], student[comb[2]])
+    result.append(total)
+
+print(max(result))
